fde8e5df3c88ecceca83347672c3b1b4548b3a1c
James Barboza <jamesbarboza720@gmail.com>
2026-03-27 16:54:59 +0530

    Revert "feat: lead assignment" (#19)

--- src/services/lead/lead.service.ts
+++ src/services/lead/lead.service.ts
@@ -1,6 +1,5 @@
 import { AxiosInstance } from "axios";
 import {
-    AssignLeadPayload,
     CreateLeadFieldRequest,
     CreateLeadRequest,
     GetLeadsParams,
@@ -39,14 +38,6 @@ export class LeadService {
 
     async createLead(lead: CreateLeadRequest): Promise<Lead> {
         const response = await this.httpClient.post('/api/v1/leads', lead);
-        return response.data;
-    }
-
-    async assignLead(leadId: string, payload: AssignLeadPayload): Promise<Lead> {
-        const response = await this.httpClient.patch(
-            `/api/v1/leads/${leadId}/assign`,
-            payload
-        );
         return response.data;
     }
 
